2ed240094814e1aa5e647e3342aebcae0450f9d1
Fadi <Fadi@10.0.0.22>
2024-03-01 15:28:00 +0200

    WeakMethod3a_Statement_And_Branch_Coverage Test

--- Statment_Brancch_coverage/src/test/java/Statment_Brancch_coverage/Statment_Brancch_coverage/TestSuite.java
+++ Statment_Brancch_coverage/src/test/java/Statment_Brancch_coverage/Statment_Brancch_coverage/TestSuite.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({
-	WeakMethod2b.class,
+	WeakMethod3a_Statement_And_Branch_Coverage.class,
 })
 public class TestSuite {
 	
