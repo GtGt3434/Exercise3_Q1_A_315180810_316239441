31d90d0c44b17629569568839ca1ca81659e22d7
Fadi <Fadi@10.0.0.22>
2024-03-01 14:08:21 +0200

    JRE 17 was updated to JRE 1.7 because I've brought a new laptop
    I had to configure Eclipse again

--- Statment_Brancch_coverage/src/test/java/Statment_Brancch_coverage/Statment_Brancch_coverage/TestSuite.java
+++ Statment_Brancch_coverage/src/test/java/Statment_Brancch_coverage/Statment_Brancch_coverage/TestSuite.java
@@ -6,11 +6,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({
-	WeakMethod1a.class, WeakMethod1b.class,
-	WeakMethod2a.class, WeakMethod2b.class,
-	WeakMethod3a_Statement_And_Branch_Coverage.class,
-	WeakMethod3b_Branch_Coverage.class,
-	WeakMethod4a.class, WeakMethod4b.class
+	WeakMethod1a.class,
 })
 public class TestSuite {
 	
--- Statment_Brancch_coverage/src/test/java/Statment_Brancch_coverage/Statment_Brancch_coverage/WeakMethod1a.java
+++ Statment_Brancch_coverage/src/test/java/Statment_Brancch_coverage/Statment_Brancch_coverage/WeakMethod1a.java
@@ -2,8 +2,6 @@ package Statment_Brancch_coverage.Statment_Brancch_coverage;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class WeakMethod1a {
 	/**
 	 * Rigorous Test :-)
