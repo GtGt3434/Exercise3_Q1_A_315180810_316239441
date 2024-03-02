8c1c4cf2601529c493abd0af0d2e64341241fe49
Fadi <Fadi@10.0.0.22>
2024-03-02 16:15:47 +0200

    Added documentation for WeakClass

--- Statment_Brancch_coverage/src/main/java/Statment_Brancch_coverage/Statment_Brancch_coverage/WeakClass.java
+++ Statment_Brancch_coverage/src/main/java/Statment_Brancch_coverage/Statment_Brancch_coverage/WeakClass.java
@@ -1,6 +1,14 @@
 package Statment_Brancch_coverage.Statment_Brancch_coverage;
 
 public class WeakClass {
+	/**
+	 * Computes the result of division and returns 1 if the result is positive,
+	 * otherwise -1.
+	 *
+	 * @param x the numerator
+	 * @param y the denominator
+	 * @return 1 if division result is positive, otherwise -1
+	 */
 	public static int weakMethod1(int x, int y) {
 		if (y == 2)
 			y += 1;
@@ -11,6 +19,14 @@ public class WeakClass {
 			return -1;
 	}
 
+	/**
+	 * Computes the result of division and returns 1 if the result is positive, -1
+	 * if negative, and 0 if zero.
+	 *
+	 * @param x the numerator
+	 * @param y the denominator
+	 * @return 1 if division result is positive, -1 if negative, and 0 if zero
+	 */
 	public static int weakMethod2(int x, int y) {
 		if (y == 1)
 			y += 1;
@@ -24,6 +40,14 @@ public class WeakClass {
 		}
 	}
 
+	/**
+	 * Computes the result of division and returns 1 if the result is positive,
+	 * otherwise -1.
+	 *
+	 * @param x the numerator
+	 * @param y the denominator
+	 * @return 1 if division result is positive, otherwise -1
+	 */
 	public static int weakMethod3(int x, int y) {
 		if (y == 3)
 			y += 3;
@@ -37,6 +61,15 @@ public class WeakClass {
 
 	}
 
+	/**
+	 * Computes the result of division and returns the quotient if both x and y are
+	 * positive, -1 if both x and y are negative, and 0 otherwise.
+	 *
+	 * @param x the numerator
+	 * @param y the denominator
+	 * @return the quotient if both x and y are positive, -1 if both x and y are
+	 *         negative, and 0 otherwise
+	 */
 	public static double weakMethod4(int x, int y) {
 		if (x > 0 || y > 0)
 			return x / y;
