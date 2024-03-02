56b86fa6e2e1199d25b152a4172fd895158cb73a
Fadi <Fadi@10.0.0.22>
2024-03-02 23:34:56 +0200

    WeakMethod4b was changed from 100% to less than 100% condition coverage
    At the beginning we thought that it was required to cover 100% of the
    conditions while not exposing the error. But, later we realized that it
    was required to cover less than 100% while exposing the error. Also a
    documentation was added

--- Statment_Brancch_coverage/src/test/java/Statment_Brancch_coverage/Statment_Brancch_coverage/WeakMethod4b.java
+++ Statment_Brancch_coverage/src/test/java/Statment_Brancch_coverage/Statment_Brancch_coverage/WeakMethod4b.java
@@ -4,40 +4,64 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+/**
+ * This JUnit test class aims to create a test suite that achieves less than 100% condition coverage
+ * for the weakMethod4 function in the WeakClass while exposing the division by zero error.
+ */
 public class WeakMethod4b {
+
+	/**
+	 * This test method verifies that the weakMethod4 function returns the expected
+	 * result when both x and y are positive numbers.
+	 */
 	@Test
 	public void Both_Positive() {
 		double expected = 10 / 5;
 		assertEquals(expected, WeakClass.weakMethod4(10, 5), 0.001);
 	}
 
+	/**
+	 * This test method verifies that the weakMethod4 function returns the expected
+	 * result when both x and y are negative numbers.
+	 */
 	@Test
 	public void Both_Negative() {
 		assertEquals(-1.0, WeakClass.weakMethod4(-10, -5), 0.001);
 	}
 
+	/**
+	 * This test method verifies that the weakMethod4 function returns the expected
+	 * result when x is negative and y is positive.
+	 */
 	@Test
 	public void First_Negative() {
 		double expected = -10 / 5;
 		assertEquals(expected, WeakClass.weakMethod4(-10, 5), 0.001);
 	}
 
+	/**
+	 * This test method verifies that the weakMethod4 function returns the expected
+	 * result when x is positive and y is negative.
+	 */
 	@Test
 	public void Second_Negative() {
 		double expected = -10 / 5;
 		assertEquals(expected, WeakClass.weakMethod4(10, -5), 0.001);
 	}
 
-	@Test
-	public void AtLeastOneZero_result() {
-		assertEquals(0, WeakClass.weakMethod4(0, 0), 0.001);
-	}
-
+	/**
+	 * This test method verifies that the weakMethod4 function returns the expected
+	 * result when x is zero and y is negative.
+	 */
 	@Test
 	public void First_Zero_Seocond_Negative() {
 		assertEquals(0, WeakClass.weakMethod4(0, -5), 0.001);
 	}
 
+	/**
+	 * This test method verifies that the weakMethod4 function returns the expected
+	 * result when x is negative and y is zero.
+	 */
 	@Test
 	public void First_Negative_Seocond_Zero() {
 		assertEquals(0, WeakClass.weakMethod4(-10, 0), 0.001);
